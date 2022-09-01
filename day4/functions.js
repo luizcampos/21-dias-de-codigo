d74d9e3058309cffccc83bf23391848af87d5364
luizcampos <felipe_luiz96@yahoo.com>
2022-09-01 06:29:21 -0300

    dia 4 finalizado

--- day4/functions.js
+++ day4/functions.js
@@ -1,54 +1,132 @@
-var valueView = document.querySelector(".viewfinder");
+const valueView = document.querySelector(".viewfinder");
+const previousView = document.querySelector(".previous");
+const buttons = document.querySelectorAll("#buttons-container button");
 
-var btn1 = document.querySelector("#symbol1"); //Number 1
-var btn2 = document.querySelector("#symbol2"); //Number 2
-
-var btnSomar = document.querySelector("#symbolSum"); 
-var btnResul = document.querySelector("#symbolResul"); 
-
-var previewResul, tamanhoValueView, lastNumber="";
-
-btn1.addEventListener("click", function(){ //Number 1
-    valueView.innerHTML += btn1.innerHTML;
-});
-
-btn2.addEventListener("click", function(){ //Number 1
-    valueView.innerHTML += btn2.innerHTML;
-});
-
-btnSomar.addEventListener("click", function(){  //+
-    
-    tamanhoValueView = valueView.innerHTML.length;
-
-    if(valueView.innerHTML == "")
-        valueView.innerHTML = "";
-    else if (
-        (valueView.innerHTML.substring(tamanhoValueView-1, tamanhoValueView) == "+") ||
-        (valueView.innerHTML.substring(tamanhoValueView-1, tamanhoValueView) == "-") ||
-        (valueView.innerHTML.substring(tamanhoValueView-1, tamanhoValueView) == "x") ||
-        (valueView.innerHTML.substring(tamanhoValueView-1, tamanhoValueView) == "/") 
-    ){
-        valueView.innerHTML += "";
+class Calculator{
+    constructor(valueView, previousView){
+        this.valueView = valueView;
+        this.previousView = previousView;
+        this.currentOperation = "";
     }
-    else
-    {
-        for(var i = valueView.innerHTML.length; i > 0; i--){ //roda procurando outros sinais antes para calcular apenas o último valor
-            if ((valueView.innerHTML.substring(i-1, i) != "+") ||
-                (valueView.innerHTML.substring(i-1, i) != "-") ||
-                (valueView.innerHTML.substring(i-1, i) != "x") ||
-                (valueView.innerHTML.substring(i-1, i) != "/"))
-            {
-                lastNumber += valueView.innerHTML.substring(i-1, i);
-                console.log(lastNumber);
-                console.log("======");
-            }
+
+    addDigit(digit){
+
+        if(digit === "." && this.valueView.innerText.includes(".")){
+            return;
         }
-        valueView.innerHTML += btnSomar.innerHTML;  
+
+        this.currentOperation = digit;
+        this.updateScreen();
     }
-});
 
-btnResul.addEventListener("click", function(){  //=
+    processOperation(operation){
 
-    const resul = valueView.innerHTML;
-    alert(resul);
+        //checar se a viewFinder está vazia
+        if(this.valueView.innerText === "" && operation !== "C"){
+            if(this.previousView.innerText !== ""){
+                //Muda operação
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+        let operationValue;
+        const previousOp = +this.previousView.innerText.split(" ")[0];
+        const current = +this.valueView.innerText;
+
+        switch(operation){
+            case '+':
+                operationValue = previousOp + current;
+                this.updateScreen(operationValue, operation, current, previousOp)
+                break;
+            case '-':
+                operationValue = previousOp - current;
+                this.updateScreen(operationValue, operation, current, previousOp)
+                break;
+            case '/':
+                operationValue = previousOp / current;
+                this.updateScreen(operationValue, operation, current, previousOp)
+                break;
+            case '*':
+                operationValue = previousOp * current;
+                this.updateScreen(operationValue, operation, current, previousOp)
+                break;
+            case 'DEL':
+                this.processDelOperator();
+                break;
+            case 'CE':
+                this.processClearCurrentOperation();
+                break;
+            case 'C':
+                this.processClearAll();
+                break;
+            case '=':
+                this.processResul();
+                break;
+            default:
+                return;
+        }
+    }
+
+    updateScreen(
+        operationValue = null, 
+        operation = null,
+        current = null, 
+        previousOp = null){
+        
+            if(operationValue === null){
+                this.valueView.innerText += this.currentOperation;
+            } else {
+                //Checar se valor é zero
+                if (previousOp === 0){
+                    operationValue = current;
+                }
+
+                this.previousView.innerText = `${operationValue} ${operation}`;
+                this.valueView.innerText = "";
+            }
+    }
+
+    changeOperation(operation){
+        const mathOperation = ["*", "/", "+", "-"];
+
+        if(!mathOperation.includes(operation)){
+            return;
+        }
+
+        this.previousView.innerText = this.previousView.innerText.slice(0, -1) + operation;
+    }
+
+    //Deletar o último dígito
+    processDelOperator(){
+        this.valueView.innerText = this.valueView.innerText.slice(0, -1);
+    }
+
+    processClearCurrentOperation(){
+        this.valueView.innerText = "";
+    }
+
+    processClearAll(){
+        this.valueView.innerText = "";
+        this.previousView.innerText = "";
+    }
+
+    processResul(){
+        const operation = previousView.innerText.split(" ")[1];
+        this.processOperation(operation);
+    }
+}
+
+const calc = new Calculator(valueView,previousView);
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){ //+value converte para number
+            calc.addDigit(value);
+        } else {
+            calc.processOperation(value);
+        }
+    })
 });
